a8ded6af599e1cd396b860de433443c990a2879a
diegopuerto24 <diego.puerto@gmail.com>
2026-06-02 17:44:41 -0600

    Hide commercial chrome in portal routes

--- app/layout.js
+++ app/layout.js
@@ -1,28 +1,12 @@
 import "./globals.css";
-import SiteHeader from "../components/SiteHeader";
+import ChromeSwitch from "../components/ChromeSwitch";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
       <body>
-        <SiteHeader />
-        <main>{children}</main>
-
-        <footer className="siteFooter">
-          <div className="container footerInner">
-            <div>
-              <strong>MC&amp;DJ Consultores y Evaluadores Profesionales</strong>
-              <div className="p-muted">Mérida, Yucatán · México</div>
-            </div>
-            <div className="footerLinks">
-              <a href="/aviso-de-privacidad">Aviso de Privacidad</a>
-              <a href="mailto:conecta@mcydj.mx">conecta@mcydj.mx</a>
-            </div>
-          </div>
-        </footer>
+        <ChromeSwitch>{children}</ChromeSwitch>
       </body>
     </html>
   );
 }
-
-
